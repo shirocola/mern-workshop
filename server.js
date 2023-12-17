f31ee4e6f5ba3d7afe85d9e17572b5ff13584864
shirocola <kitpivatpiromgit@gmail.com>
2023-12-17 21:18:57 +0700

    refactor

--- server.js
+++ server.js
@@ -2,23 +2,37 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
-require("dotenv").config()
-const blogRoute = require('./routes/blog')
-const authRoute = require('./routes/auth')
+require("dotenv").config();
+const blogRoute = require('./routes/blog');
+const authRoute = require('./routes/auth');
 
-const app = express()
+const app = express();
 
-mongoose.connect(process.env.DB_URL).then(()=>console.log("connection success"))
-.catch((err)=>console.log(err))
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
 
-//middleware
-app.use(express.json())
-app.use(cors())
-app.use(morgan("dev"))
+// Routes (register routes before the server starts listening)
+app.use("/api", blogRoute);
+app.use("/api", authRoute);
 
-//route
-app.use("/api",blogRoute)
-app.use("/api",authRoute)
+const port = process.env.PORT || 7000;
 
-const port = process.env.PORT || 7000
-app.listen(port,()=>console.log(`start server in port ${port}`))
+// MongoDB Connection
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log("connection success");
+    // Start the server after successful DB connection
+    app.listen(port, () => {
+      console.log(`Server started on port ${port}`);
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1); // Exit the process with failure
+  }
+};
+
+// Connect to DB then listen
+connectDB();
