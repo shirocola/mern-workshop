3a9bc1c6dc578123210a6dc12a0320504d377502
shirocola <kitpivatpiromgit@gmail.com>
2023-12-17 17:10:51 +0700

    loginController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,12 @@
+const jwt = require('jsonwebtoken');
+
 exports.login=(req, res)=>{
-    res.json({
-        data: "login success"
-    })
+   const {username, password} = req.body;
+   if(password === process.env.PASSWORD_ADMIN){
+        const jwtToken = jwt.sign({username}, process.env.JWT_SECRET, {expiresIn: '1d'});
+        return res.json({jwtToken, username});
+   }
+    else{
+         return res.status(400).json({error:"Invalid password"});
+    }
 }
